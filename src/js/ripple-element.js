05d2fe99d3088992884f5afb15e55008abe47c1f
Gerson Lázaro <GersonLazaro@GersonLazaro.com>
2016-01-18 16:51:44 -0500

    Inicializando libreria

---
+++ src/js/ripple-element.js
@@ -0,0 +1,8 @@
+
+
+class RippleElement {
+
+	constructor() {
+		
+	}
+}
---
+++ src/js/ripple-lite.js
@@ -0,0 +1,24 @@
+/**
+*	ripple-lite.js
+*	Efecto Ripple (Material Design) liviano y sin jquery
+*	@author: Gerson Lázaro <http://www.gersonlazaro.com>
+*	@Licencia: MIT
+*/
+
+'use strict';
+
+class Ripple {
+
+	constructor() {
+		this.elements = [];
+		this.init();
+	}
+
+	init() {
+		let selected = document.querySelectedAll(".ripple");
+
+		for(i = 0; i < selected.length; i++){
+			this.elements[i] = new RippleElement(selected[i]);
+		}
+	}
+}
